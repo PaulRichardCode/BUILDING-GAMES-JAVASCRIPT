e026e5794d4ceed89ce05787b887d735a028e76e
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2022-12-11 12:24:19 +0100

    finally

--- Rock-Paper-Scissors/app.js
+++ Rock-Paper-Scissors/app.js
@@ -6,6 +6,7 @@ const result = document.querySelector("#result");
 
 let mommy;
 let computer;
+let answer;
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -30,7 +31,39 @@ function getComputerChoice() {
 }
 
 function getResult() {
-  if (computerChoice.innerText === "rock" && userChoice.innerText === "") {
-    console.log("MOVE ON BROTHER");
+  if (computerChoice.innerHTML === userChoice.innerHTML) {
+    answer = "It a Draw";
+  } else if (
+    computerChoice.innerText === "rock" &&
+    userChoice.innerText === "paper"
+  ) {
+    answer = "You Win(Congrats!)";
+  } else if (
+    computerChoice.innerText === "rock" &&
+    userChoice.innerText === "scissors"
+  ) {
+    answer = "You lose(Try again!)";
+  } else if (
+    computerChoice.innerText === "paper" &&
+    userChoice.innerText === "rock"
+  ) {
+    answer = "You lose(Try again!)";
+  } else if (
+    computerChoice.innerText === "paper" &&
+    userChoice.innerText === "scissors"
+  ) {
+    answer = "You Win(Congrats!)";
+  } else if (
+    computerChoice.innerText === "scissors" &&
+    userChoice.innerText === "rock"
+  ) {
+    answer = "You Win(Congrats!)";
+  } else if (
+    computerChoice.innerText === "scissors" &&
+    userChoice.innerText === "paper"
+  ) {
+    answer = "You lose(Try again!)";
   }
+
+  result.innerHTML = answer;
 }
