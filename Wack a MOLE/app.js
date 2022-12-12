391c712c1711631491afde9c48292dccb5c392e9
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2022-12-12 12:36:34 +0100

    new project

---
+++ Wack a MOLE/app.js
@@ -0,0 +1,31 @@
+const cardArray = [
+  { name: "fries", img: "img/fries.jpg" },
+  {
+    name: "hamburger",
+    img: "img/hamburger.jpg",
+  },
+  {
+    name: "hotdog",
+    img: "img/hotdog.jpg",
+  },
+  {
+    name: "icecream",
+    img: "img/ice cream.jpg",
+  },
+  {
+    name: "milkshake",
+    img: "img/milkshake.jpg",
+  },
+  {
+    name: "pizza",
+    img: "img/pizza.jpg",
+  },
+  {
+    name: "white",
+    img: "img/white.jpg",
+  },
+  {
+    name: "blank",
+    img: "img/blank.jpg",
+  },
+];
