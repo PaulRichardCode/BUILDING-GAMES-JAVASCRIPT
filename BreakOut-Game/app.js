aac498481b1fcd5ccf463a8e27fdbae0429b46e7
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2022-12-16 21:32:38 +0100

    game

--- BreakOut-Game/app.js
+++ BreakOut-Game/app.js
@@ -1,6 +1,93 @@
 const grid = document.querySelector(".grid");
-const block = document.createElement("div");
+const blockWidth = 100;
+const blockHeight = 20;
+const userStart = [230, 10];
+const boardWidth = 560;
+let currentPosition = userStart;
+//creare block
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+  }
+}
 
-block.classList.add("block");
-grid.appendChild(block);
-block.style.left = "100px";
+//all my blocks
+
+const blocks = [
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210),
+];
+
+//draw all my blocks
+function addBlocks() {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.style.left = blocks[i].bottomLeft[0] + "px";
+    block.style.bottom = blocks[i].bottomLeft[1] + "px";
+    console.log(blocks[i].bottomLeft);
+    grid.appendChild(block);
+  }
+}
+
+addBlocks();
+
+//add user
+
+const user = document.createElement("div");
+user.classList.add("user");
+drawUser();
+grid.appendChild(user);
+
+//drawUser
+
+function drawUser() {
+  user.style.left = currentPosition[0] + "px";
+  user.style.bottom = currentPosition[1] + "px";
+}
+
+//move user
+
+function moveUser(e) {
+  switch (e.key) {
+    case "ArrowLeft":
+      if (currentPosition[0] > 0) {
+        currentPosition[0] -= 10;
+        drawUser();
+      }
+      break;
+
+    case "ArrowRight":
+      if (currentPosition[0] < boardWidth - blockWidth) {
+        currentPosition[0] += 10;
+        drawUser();
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+document.addEventListener("keydown", moveUser);
+
+//add ball
+
+const ball = document.createElement("div");
+ball.classList.add("ball");
